974996ff2b90921d954bcd07d518a8a521d26a4e
Suraj Patil <surajspatil824@gmail.com>
2025-02-26 00:26:45 +0530

    add another function

--- src/main/java/in/sbms/config/SwaggerConfig.java
+++ src/main/java/in/sbms/config/SwaggerConfig.java
@@ -4,5 +4,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    public void configFun(){}
+    //public void configFun(){}
+	//public void configFun2(){}
 }
